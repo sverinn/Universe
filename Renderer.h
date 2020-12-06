87359ff6be379de5e0a81837d8c66fd20c95b7ef
Dmitry Vinyar <dimavinyar@gmail.com>
2020-12-06 20:05:16 +0300

    Remove DirectX9 from project Preparing to rewrite renderer for DirectX12

--- Renderer.h
+++ Renderer.h
@@ -1,9 +1,1 @@
 #pragma once
-#include <d3dx9.h>
-
-size_t RenderPointlist(
-    IDirect3DDevice9*& d3dDevice,
-    LPDIRECT3DVERTEXBUFFER9& pVertexObject,
-    LPDIRECT3DVERTEXDECLARATION9& vertexDecl,
-    void*& pVertexBuffer,
-    std::vector<PhysicalObject*>& ObjectReg);
